d7c342815a78dd639dfe0473353b8f0439b8d27e
Blake <blake@futchi.com>
2020-02-21 14:49:29 +0100

    more console.log checks on dom elements

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const userScore = 0;
 const computerScore = 0;
-const userScoreSpan = document.getElementById("user-score");
-const computerScoreSpan = document.getElementById("computer-score");
-
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const score_div = document.querySelector(".score");
+const results_div = document.querySelector(".results");
 
 console.log(userScore + " points for user");
 console.log(computerScore + " points for computer");
 
-console.log(userScoreSpan);
-console.log(computerScoreSpan);
+console.log(userScore_span);
+console.log(computerScore_span);
+
+console.log(score_div);
+console.log(results_div);
