2d9d7feb01b27eba11a9b2bd1cf6337306f412bb
Blake <blake@futchi.com>
2020-02-21 14:41:22 +0100

    can set user / computer score to 0 and grab from html and show number value in console

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello")
+const userScore = 0;
+const computerScore = 0;
+const userScoreSpan = document.getElementById("user-score");
+const computerScoreSpan = document.getElementById("computer-score");
+
+
+console.log(userScore + " points for user");
+console.log(computerScore + " points for computer");
+
+console.log(userScoreSpan);
+console.log(computerScoreSpan);
