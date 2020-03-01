19330e5ef9243e35d206c1adaf0c74e3311c2d0a
Blake <blake@futchi.com>
2020-03-01 13:16:01 +0100

    added more test scaffold

---
+++ cypress/integration/resultsCanBeDisplayed.spec.js
@@ -0,0 +1,18 @@
+describe("Page can show results", () => {
+  beforeEach('round', () => {
+    cy.visit("http://localhost:3001/");
+  })
+
+  it('the game is a tie', () => {
+
+  });
+
+  it('user wins', () => {
+
+  });
+
+  it('computer wins', () => {
+
+  });
+
+});
--- cypress/integration/userCanSeeDom.spec.js
+++ cypress/integration/userCanSeeDom.spec.js
@@ -1,7 +1,8 @@
 describe("User can open game", () => {
-  beforeEach('round', () => {
+  beforeEach('match', () => {
     cy.visit("http://localhost:3001/");
   })
+
   it("displays dom variables", () => {
     cy.get("h1").should('be.visible');
     cy.get("#user").should('contain', 'User');
@@ -10,7 +11,8 @@ describe("User can open game", () => {
     cy.get("#computer-score").contains('0');
     cy.get("#message").should('contain', 'Make your move. Choose wisely');
   })
-  it("user can click element", () => {
+
+  it("user can see rock paper scissors elements and click on them", () => {
     cy.get("#r").click();
     cy.get("#p").click();
     cy.get("#s").click();
