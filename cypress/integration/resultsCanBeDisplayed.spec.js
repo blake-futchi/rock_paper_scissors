27ad7d5cf0adab57ce613a87a5cb5d2500b11315
Blake <blake@futchi.com>
2020-03-05 11:54:40 +0100

    deleted master hand spec file, updated result spec with multi stubbing, added master hand id in html, removed unecessary cypress command

--- cypress/integration/resultsCanBeDisplayed.spec.js
+++ cypress/integration/resultsCanBeDisplayed.spec.js
@@ -1,37 +1,89 @@
 describe("Page can show results", () => {
-  beforeEach('round', () => {
-    cy.stubOutRandom(0)
-  })
-  describe('the game is a tie', () => {
-    beforeEach('round', () => {
-      cy.stub(Math, 'floor').returns(0)
-      cy.get("#r").click();
+  describe('when computer picks rock', () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3001", {
+        onBeforeLoad: win => {
+          cy.stub(win.Math, 'floor').returns(0)
+        }
+      })
     })
-    it('displays the score', () => {
+    it('user picks rock equals a draw', () => {
+      cy.get("#r").click();
       cy.get('#user-score').should('contain', '0')
       cy.get('#computer-score').should('contain', '0')
     });
-  });
-  
-  describe('the user wins', () => {
-    beforeEach('round', () => {
-      cy.stub(Math, 'floor').returns(0)
+    it('user picks scissor equals a computer win', () => {
+      cy.get("#s").click();
+      cy.get('#user-score').should('contain', '0')
+      cy.get('#computer-score').should('contain', '1')
+    });
+    it('user picks paper equals a user win', () => {
       cy.get("#p").click();
-    }) 
-    it('displays the score', () => {
+      cy.get('#user-score').should('contain', '1')
+      cy.get('#computer-score').should('contain', '0')
+    });
+    it('user picks master hand equals auto win', () => {
+      cy.get("#m").click();
       cy.get('#user-score').should('contain', '1')
       cy.get('#computer-score').should('contain', '0')
     });
   });
-  describe('the computer wins', () => {
-    beforeEach('round', () => {
-      cy.stub(Math, 'floor').returns(0)
-      cy.get("#s").click();
-    }) 
-    it('displays the score', () => {
+  describe('when computer picks paper', () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3001", {
+        onBeforeLoad: win => {
+          cy.stub(win.Math, 'floor').returns(1)
+        }
+      })
+    })
+    it('user picks rock equals a computer win', () => {
+      cy.get("#r").click();
       cy.get('#user-score').should('contain', '0')
       cy.get('#computer-score').should('contain', '1')
-      });
+    });
+    it('user picks scissor equals a user win', () => {
+      cy.get("#s").click();
+      cy.get('#user-score').should('contain', '1')
+      cy.get('#computer-score').should('contain', '0')
+    });
+    it('user picks paper equals a draw', () => {
+      cy.get("#p").click();
+      cy.get('#user-score').should('contain', '0')
+      cy.get('#computer-score').should('contain', '0')
+    });
+    it('user picks master hand equals auto win', () => {
+      cy.get("#m").click();
+      cy.get('#user-score').should('contain', '1')
+      cy.get('#computer-score').should('contain', '0')
+    });
+  });
+  describe('when computer picks scissor', () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3001", {
+        onBeforeLoad: win => {
+          cy.stub(win.Math, 'floor').returns(2)
+        }
+      })
+    })
+    it('user picks rock equals a user win', () => {
+      cy.get("#r").click();
+      cy.get('#user-score').should('contain', '1')
+      cy.get('#computer-score').should('contain', '0')
+    });
+    it('user picks scissor equals a draw', () => {
+      cy.get("#s").click();
+      cy.get('#user-score').should('contain', '0')
+      cy.get('#computer-score').should('contain', '0')
+    });
+    it('user picks paper equals a computer win', () => {
+      cy.get("#p").click();
+      cy.get('#user-score').should('contain', '0')
+      cy.get('#computer-score').should('contain', '1')
+    });
+    it('user picks master hand equals auto win', () => {
+      cy.get("#m").click();
+      cy.get('#user-score').should('contain', '1')
+      cy.get('#computer-score').should('contain', '0')
+    });
   });
 });
-
